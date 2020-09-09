9548342ea0215d7056196833ac1e3120129b9f5d
fausio luis <luisfausio@gmail.com>
2020-09-09 22:52:38 +0200

    IobjectRepository

---
+++ EmployeeBlazor.API/Interface/IobjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeBlazor.Model;
+
+namespace EmployeeBlazor.API.Interface
+{
+    interface IobjectRepository
+    {
+        Task<IEnumerator<object>> GetAllModel();
+        Task<object> AddModel(object obj);
+        Task<object> UpdateModel(object obj);
+        void DeleteModel(int objID);
+    }
+}
