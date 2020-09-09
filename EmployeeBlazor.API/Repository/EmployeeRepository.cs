0e4af2a466880a60ef31ec3d0a9720cb9f8c778c
fausio luis <luisfausio@gmail.com>
2020-09-09 22:52:45 +0200

    EmployeeRepository

---
+++ EmployeeBlazor.API/Repository/EmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeBlazor.API.Repository
+{
+    public class EmployeeRepository 
+    {
+    }
+}
