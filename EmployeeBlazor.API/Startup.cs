175b210b19fabfa643798780e257735ae9ed1ae0
fausio luis <luisfausio@gmail.com>
2020-09-09 19:29:04 +0200

    UseSqlServer support

--- EmployeeBlazor.API/Startup.cs
+++ EmployeeBlazor.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeBlazor.API.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace EmployeeBlazor.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppdbCtxt>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));
             services.AddControllers();
         }
 
