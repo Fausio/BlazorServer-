3f5b2c399da761054543f2468a157c5fb3a8f66e
fausio luis <luisfausio@gmail.com>
2020-09-09 18:12:37 +0200

    ctxt class with dbsets

---
+++ EmployeeBlazor.API/Model/AppdbCtxt.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeBlazor.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+
+namespace EmployeeBlazor.API.Model
+{
+    public class AppdbCtxt : DbContext
+    {
+        public AppdbCtxt(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Employee> GetEmployee { get; set; }
+
+     
+    }
+}
