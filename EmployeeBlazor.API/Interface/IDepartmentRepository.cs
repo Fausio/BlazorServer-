4370e5621a22487f52c872629699b67a8af72f0d
fausio luis <luisfausio@gmail.com>
2020-09-10 00:02:09 +0200

    IDepartmentRepository

---
+++ EmployeeBlazor.API/Interface/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeBlazor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeBlazor.API.Interface
+{
+ public  interface IDepartmentRepository
+    {
+      Task<IEnumerable<Department>>   GetAllModels();
+        Task<Department>  GetModelById(int ModelId);
+    }
+}
