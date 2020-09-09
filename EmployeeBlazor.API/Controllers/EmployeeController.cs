47e4ee1790939eed2957913dd6bedff9d73d1491
fausio luis <luisfausio@gmail.com>
2020-09-10 00:45:25 +0200

    EmployeeController  [Route("api/[controller]")]     [ApiController]GetAllEmployee

---
+++ EmployeeBlazor.API/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeBlazor.API.Interface;
+using EmployeeBlazor.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeBlazor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+        public EmployeeController(IEmployeeRepository employee)
+        {
+            this.employeeRepository = employee;
+        }
+
+        public async Task<ActionResult> GetAllEmployee()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetAllModel());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao ler os dados da base ");
+            }
+          
+        }
+    }
+}
