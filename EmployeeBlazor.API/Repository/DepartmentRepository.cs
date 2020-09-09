49d50a2b4f19167ffa60d05caef4ab04d92d84a2
fausio luis <luisfausio@gmail.com>
2020-09-10 00:02:22 +0200

    DepartmentRepository

---
+++ EmployeeBlazor.API/Repository/DepartmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeBlazor.API.Interface;
+using EmployeeBlazor.API.Model;
+using EmployeeBlazor.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeBlazor.API.Repository
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public DepartmentRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.db = applicationDbContext;
+        }
+        public async Task<IEnumerable<Department>> GetAllModels()
+        {
+            return await db.Department.ToListAsync();
+        }
+
+        public async Task<Department> GetModelById(int ModelId)
+        {
+            Task<Department> result = db.Department.FirstOrDefaultAsync(x => x.DepartmentId == ModelId);
+            if (result != null)
+            {
+                return await result;
+            }
+            else
+            {
+                return  null;
+            }
+        }
+    }
+}
