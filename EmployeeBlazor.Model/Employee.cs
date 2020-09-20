52a595a4dc68c911eaff4ffbcca666030dbd694f
fausio luis <luisfausio@gmail.com>
2020-09-20 17:43:06 +0200

    DataAnnotations  and name required to Employee

--- EmployeeBlazor.Model/Employee.cs
+++ EmployeeBlazor.Model/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace EmployeeBlazor.Model
     public class Employee
     {
         public int EmployeeId { get; set; }
+
+        [Required( ErrorMessage ="O nome do funcionario é obrigatório")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
