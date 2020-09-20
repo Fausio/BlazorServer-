1ba1390c6c0de7f692f4c3b83c1b6ebd20df9e75
fausio luis <luisfausio@gmail.com>
2020-09-20 17:08:32 +0200

    CreateEmployee Action

--- EmployeeBlazor.API/Controllers/EmployeeController.cs
+++ EmployeeBlazor.API/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EmployeeBlazor.API.Interface;
 using EmployeeBlazor.API.Repository;
+using EmployeeBlazor.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,21 @@ namespace EmployeeBlazor.API.Controllers
             this.employeeRepository = employee;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                return Ok(await employeeRepository.AddModel(employee));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao adicionar o modelo na dados da base ");
+            }
+        }
+
+        [HttpGet]
         public async Task<ActionResult> GetAllEmployee()
         {
             try
@@ -29,7 +45,27 @@ namespace EmployeeBlazor.API.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao ler os dados da base ");
             }
-          
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetEmployeeById(int id)
+        {
+            try
+            {
+                var result = Ok(await employeeRepository.GetAllModelById(id));
+                if (result != null)
+                {
+                    return result;
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao ler os dados da base ");
+            }
         }
     }
 }
