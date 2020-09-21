192ba977071b87c17f6aff9283b28f7f0c012f82
fausio luis <luisfausio@gmail.com>
2020-09-21 11:23:07 +0200

    EmployeeDetailBase

---
+++ BlazorEmployee.Web/Pages/EmployeeDetailBase.cs
@@ -0,0 +1,28 @@
+﻿using BlazorEmployee.Web.Services;
+using EmployeeBlazor.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Web.Pages
+{
+    public class EmployeeDetailBase : ComponentBase
+    {
+
+       public  IEmployeeServices EmployeeServices { get; set; }
+
+        public Employee Employee { get; set; }
+
+
+
+
+
+        protected override  async Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+    }    
+}
