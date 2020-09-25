4365056e893590ef04cf877748b343b99079c857
fausio luis <luisfausio@gmail.com>
2020-09-25 09:27:47 +0200

    DisplayEmployee props

--- BlazorEmployee.Web/Pages/DisplayEmployeeBase.cs
+++ BlazorEmployee.Web/Pages/DisplayEmployeeBase.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using EmployeeBlazor.Model;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorEmployee.Web.Pages
 {
-    public class DisplayEmployeeBase
+    public class DisplayEmployeeBase :ComponentBase
     {
+
+        [Parameter]
+        public Employee Employee { get; set; }
+
+        [Parameter]
+        public bool ShowFooter { get; set; }
     }
 }
