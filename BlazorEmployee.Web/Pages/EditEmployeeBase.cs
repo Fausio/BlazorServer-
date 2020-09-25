9faa57b61f68451b7b86a56df528c369f80c7813
fausio luis <luisfausio@gmail.com>
2020-09-25 12:25:39 +0200

    edit base class

---
+++ BlazorEmployee.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,28 @@
+﻿using BlazorEmployee.Web.Services;
+using EmployeeBlazor.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        protected IEmployeeServices employeeServices { get; set; }
+
+        [Parameter]
+        public Employee Employee { get; set; } = new Employee();
+
+        public int Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await employeeServices.GetEmployeeById(Id);
+        }
+
+
+    }
+}
