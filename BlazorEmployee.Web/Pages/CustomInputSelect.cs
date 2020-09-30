e11c6bd6af23d7a8a5b1bf44b55e213b0179fcbc
fausio luis <luisfausio@gmail.com>
2020-09-30 09:09:45 +0200

    CostumInput for blazor component

---
+++ BlazorEmployee.Web/Pages/CustomInputSelect.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Web.Pages
+{
+    public class CustomInputSelect<Tvalue> : InputSelect<Tvalue>
+    {
+        protected override bool TryParseValueFromString(string value, out Tvalue result, out string validationErrorMessage)
+        {
+
+            if (typeof(Tvalue) == typeof(int))
+            {
+                if (int.TryParse(value, out var resultInt))
+                {
+                    result = (Tvalue)(object)resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = $"The selected value {value} is not a valid number.";
+                    return false;
+                }
+            }
+            else
+            {
+                return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+            }
+
+
+        }
+    }
+}
