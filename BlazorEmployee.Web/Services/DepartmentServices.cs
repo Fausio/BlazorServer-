bba5f5c65ec679bbe847fae53b0b9d09323ba153
fausio luis <luisfausio@gmail.com>
2020-09-30 09:07:03 +0200

    deparment services

---
+++ BlazorEmployee.Web/Services/DepartmentServices.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeBlazor.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Web.Services
+{
+    public class DepartmentServices : IDepartmentServices
+    {
+        private readonly HttpClient httpClient;
+        public DepartmentServices(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<Department> GetDepartmentById(int EmployeeId)
+        {
+            return await httpClient.GetJsonAsync<Department>($"api/Department/{EmployeeId}");
+        }
+
+        public async Task<List<Department>> GetDepartments()
+        {
+            return await httpClient.GetJsonAsync<List<Department>>("api/Department");
+        }
+    }
+}
---
+++ BlazorEmployee.Web/Services/IDepartmentServices.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeBlazor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Web.Services
+{
+    public interface IDepartmentServices
+    {
+        Task<List<Department>> GetDepartments();
+
+        Task<Department> GetDepartmentById(int EmployeeId);
+    }
+}
